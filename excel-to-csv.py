e0942f702b19d3ad30451e297e688bcc41b95e6e
all-back <jaycedeng1215@163.com>
2020-04-14 10:16:39 +0800

    智能转换

--- excel-to-csv.py
+++ excel-to-csv.py
@@ -1,11 +1,15 @@
 # -*- coding: UTF-8 -*-
 
 import os
+# import platform
 import pandas as pd
 import tkinter as tk
+# import openpyxl
 from tkinter import filedialog
 from tkinter import Radiobutton
 from tkinter import messagebox
+
+# platformSystem = platform.system()
 
 def selectFile():
     filePath = tk.filedialog.askopenfilename(filetypes=[('xlsx', '*.xlsx'), ('xls', '*.xls')])
@@ -34,11 +38,28 @@ def selectFile():
     if sheetType == 1:
         dataExcel = pd.read_excel(excel, sheet_name=None, index_col=0)
         for i in dataExcel:
+            # i 就是sheet名称
             transfterPath = os.path.join(path + '/', fileName + ' - ' + i + '.csv')
+            # charset = 'utf-8'
+            # if platformSystem == 'Windows':
+            #     transfterPath = transfterPath.replace('/', '\\')
+                # charset = 'gb2312'
+            # dataExcel[i].to_csv(transfterPath, encoding=charset)
             try:
-                dataExcel[i].to_csv(transfterPath, encoding='utf-8')
+                dataExcel[i].to_csv(transfterPath)
             except Exception as e:
                 tk.messagebox.showerror('错误', e)
+    elif sheetType == 2:
+        # usecols = ['自编号', '项目编码', '站点编号', '运营商', '站点名称', '所属区域']
+        allowSheets = ['本年室外', '5G专项', '非标', '本年室外进度总表', '非标清单']
+        for i in sheets:
+            if i in allowSheets:
+                try:
+                    dataExcel = pd.read_excel(excel, sheet_name=i, index_col=0)#, usecols=usecols)
+                    transfterPath = os.path.join(path + '/', fileName + ' - ' + i + '.csv')
+                    dataExcel.to_csv(transfterPath, encoding='utf-8')
+                except Exception as e:
+                    tk.messagebox.showerror('错误', e)
     tk.messagebox.showinfo('提示', '已转换完成，路径：' + path)
 
 def readSheets(filePath):
@@ -72,11 +93,13 @@ choiceTip.grid(row=2, column=0, sticky=tk.NW)
 
 
 radioEle = tk.IntVar()
-radioEle.set(1)
+radioEle.set(2)
 
 transfterLabel = tk.Label(frame, text='sheet操作：')
 transfterLabel.grid(row=0, column=0, sticky=tk.NW)
 radioBtn1 = Radiobutton(frame, text='全部转换', variable=radioEle, value=1)
 radioBtn1.grid(row=0, column=1, sticky=tk.NW)
+radioBtn2 = Radiobutton(frame, text='智能转换', variable=radioEle, value=2)
+radioBtn2.grid(row=0, column=2, sticky=tk.NW)
 
 window.mainloop()
